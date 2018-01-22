b6dafa79be7cb1fca3bd58aec2514fe9ae63d164
Rachel Johnson <rachel@squareyes.info>
2018-01-22 12:26:12 +1100

    Create email reset option and routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   # basic user attributes (name, email, password)
   before_save :downcase_email
   before_create :create_activation_digest
@@ -53,6 +53,17 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  # Sets the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_send_at, Time.zone.now)
+  end
+
+ # Sends password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 
   private
 
