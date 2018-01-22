e7b739f86b1aa03f6cb9904be4582092399d278b
Rachel Johnson <rachel@squareyes.info>
2018-01-22 11:47:27 +1100

    Generte forgot password files and link to reset and submit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'  # use your local dev host 
+  host = 'localhost:3000'  # use your local dev host \
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   # Don't care if the mailer can't send.
